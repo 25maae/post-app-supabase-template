ca26ceaf9328c65b085ed176cd634efe99aad25f
25maae <eaa25maae@students.eaaa.dk>
2026-04-23 13:44:30 +0200

    fix: correct headers formatting and clean up handleSubmit function in CreatePage component

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -5,7 +5,7 @@ import PostForm from "../components/PostForm";
 const URL = import.meta.env.VITE_SUPABASE_URL;
 const headers = {
   apikey: import.meta.env.VITE_SUPABASE_APIKEY,
-  "Content-Type": "application/json"
+  "Content-Type": "application/json",
 };
 
 export default function CreatePage() {
@@ -14,23 +14,23 @@ export default function CreatePage() {
   const [errorMessage, setErrorMessage] = useState("");
 
   async function handleSubmit(postData) {
-    // TODO:
-    // 1. Send POST request med fetch
-    // 2. Brug JSON.stringify(postData)
-    // 3. Navigér tilbage til forsiden ved succes
-    //
-    // Ekstra bagefter:
-    // - isSubmitting
-    // - try/catch
-    // - fejlbesked
-    console.log(postData, URL, headers);
+    await fetch(URL, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(postData),
+    });
+
     navigate("/");
   }
 
   return (
     <main className="app">
       <h1 className="page-title">Create Post</h1>
-      <PostForm onSubmit={handleSubmit} isSubmitting={isSubmitting} errorMessage={errorMessage} />
+      <PostForm
+        onSubmit={handleSubmit}
+        isSubmitting={isSubmitting}
+        errorMessage={errorMessage}
+      />
     </main>
   );
 }
